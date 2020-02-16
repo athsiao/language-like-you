65a7e0eb2d909186df82d6ff6b0783143a5c9794
Athena Siaotong <asiaotong@ucmerced.edu>
2020-02-16 07:58:53 -0800

    Added new assets and implemented home page

--- lly/lib/main.dart
+++ lly/lib/main.dart
@@ -6,12 +6,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+int learnerType = 0;
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Language Like You',
-      theme: ThemeData(fontFamily: 'Moon2.0'),
+      theme: ThemeData(fontFamily: 'Moon2.0-Regular'),
       home: Splash(),
     );
   }
@@ -176,21 +178,24 @@ class _LearningTestState extends State<LearningTest> {
 
   void _seeResults() {
     if (_1counter >= _2counter && _1counter >= _3counter) {
+      learnerType = 0;
       Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Splash()),
+          MaterialPageRoute(builder: (context) =>  Visual()),
         );
         print("Visual learner concluded");
     } else if (_2counter >= _1counter && _2counter >= _3counter) {
+      learnerType = 1;
       Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Splash()),
+          MaterialPageRoute(builder: (context) => Auditory()),
         );
         print("Auditory learner concluded");
     } else {
+      learnerType = 2;
       Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Splash()),
+          MaterialPageRoute(builder: (context) => Logical()),
         );
         print("Logical learner concluded");
     }
@@ -219,12 +224,19 @@ class _LearningTestState extends State<LearningTest> {
     
     return Container(
         color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 150),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
 
-            Text(questions[_questionNumber]),
+            Text(questions[_questionNumber],
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 30,
+                fontFamily: 'Moon2.0-Bold',
+                color: Colors.pink[600],
+              )
+            ),
 
             // OPTION 1
             FlatButton(
@@ -285,3 +297,341 @@ class _LearningTestState extends State<LearningTest> {
     );
   }
 }
+
+// RESULTS PAGE - VISUAL LEARNERS
+class Visual extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 80, vertical: 50),
+        child: Column(
+          children: <Widget>[
+            // logo
+            Image(
+              image: AssetImage('assets/eye.png'),
+            ),
+            Text("You are a visual learner!",
+            style: TextStyle(
+                fontFamily: "Moon2.0-Regular",
+                fontWeight: FontWeight.w300,
+                color: Colors.pink[400],
+              )
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: FlatButton(
+                onPressed: () {
+                  print("Visual Continue");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                },
+                child: Text(
+                  "learn visually",
+                  style: TextStyle(
+                    color: Colors.pink[900],
+                    fontFamily: 'Moon2.0',
+                  )
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 75),
+                color: Colors.pink[50],
+              ),
+            ),
+          ]
+        ),
+      );
+  }
+}
+
+// RESULTS PAGE - AUDITORY LEARNERS
+class Auditory extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 80, vertical: 50),
+        child: Column(
+          children: <Widget>[
+            // logo
+            Image(
+              image: AssetImage('assets/ear.png'),
+            ),
+            Text("You are an auditory learner!",
+            style: TextStyle(
+                fontFamily: "Moon2.0-Regular",
+                fontWeight: FontWeight.w300,
+                color: Colors.pink[400],
+              )
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: FlatButton(
+                onPressed: () {
+                  print("Auditory continue");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                },
+                child: Text(
+                  "learn with audio",
+                  style: TextStyle(
+                    color: Colors.pink[900],
+                    fontFamily: 'Moon2.0',
+                  )
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 75),
+                color: Colors.pink[50],
+              ),
+            ),
+          ]
+        ),
+      );
+  }
+}
+
+// RESULTS PAGE - LOGICAL LEARNERS
+class Logical extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 80, vertical: 50),
+        child: Column(
+          children: <Widget>[
+            // logo
+            Image(
+              image: AssetImage('assets/brain.png'),
+            ),
+            Text("You are a logical learner!",
+              style: TextStyle(
+                fontFamily: "Moon2.0-Regular",
+                fontWeight: FontWeight.w300,
+                color: Colors.pink[400],
+              )
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: FlatButton(
+                onPressed: () {
+                  print("Logical continue");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                },
+                child: Text(
+                  "learn logically",
+                  style: TextStyle(
+                    color: Colors.pink[900],
+                    fontFamily: 'Moon2.0',
+                  )
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 75),
+                color: Colors.pink[50],
+              ),
+            ),
+          ]
+        ),
+      );
+  }
+}
+
+// HOME PAGE ----------------------------------------------------------
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+  // 0 = visual
+  // 1 = auditory
+  // 2 = logical
+
+  List<String> types = [
+    "visually",
+    "auditorily",
+    "logically",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 35),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+
+            Row(
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/china.png'),
+                  width: 70,
+                  ),
+                Image(
+                  image: AssetImage('assets/japan.png'),
+                  width: 70,
+                  ),
+                Image(
+                  image: AssetImage('assets/add-new.png'),
+                  width: 70,
+                  ),
+              ],
+            ),
+
+            Container(
+              padding: EdgeInsets.only(top: 60),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Welcome back!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 30,
+                      fontFamily: 'Moon2.0-Bold',
+                      color: Colors.pink[600],
+                  )
+                )
+              )
+            ),
+
+            Container(
+              padding: EdgeInsets.only(top: 40),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Lesson One: Introductions",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      fontFamily: 'Moon2.0-Bold',
+                      color: Colors.pink[600],
+                  )
+                )
+              )
+            ),
+
+            Row( // first row
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/lessons/intro.png'),
+                  width: 100,
+                  ),
+                Image(
+                  image: AssetImage('assets/lessons/greeting.png'),
+                  width: 100,
+                  ),
+                Image(
+                  image: AssetImage('assets/lessons/meeting.png'),
+                  width: 100,
+                  ),
+              ],
+            ),
+
+          Container(
+              padding: EdgeInsets.only(top: 40),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Lesson Two: Times, Places, Emergency",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      fontFamily: 'Moon2.0-Bold',
+                      color: Colors.pink[600],
+                  )
+                )
+              )
+            ),
+
+            Row( // second row
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/lessons/time.png'),
+                  width: 100,
+                  ),
+                Image(
+                  image: AssetImage('assets/lessons/location.png'),
+                  width: 100,
+                  ),
+                Image(
+                  image: AssetImage('assets/lessons/emergency.png'),
+                  width: 100,
+                  ),
+              ],
+            ),
+
+            Container(
+              padding: EdgeInsets.only(top: 40),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Lesson Three: Meals of the Day",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      fontFamily: 'Moon2.0-Bold',
+                      color: Colors.pink[600],
+                  )
+                )
+              )
+            ),
+
+            Row( // third row
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/lessons/breakfast.png'),
+                  width: 100,
+                  ),
+                Image(
+                  image: AssetImage('assets/lessons/lunch.png'),
+                  width: 100,
+                  ),
+                Image(
+                  image: AssetImage('assets/lessons/dinner.png'),
+                  width: 100,
+                  ),
+              ],
+            ),
+
+            // OPTION 3
+            Container(
+              padding: EdgeInsets.only(top: 20),
+              child: FlatButton(
+              onPressed: () {
+                Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>  Welcome()),
+              );
+              },
+            child: Text(
+                  "retake the test",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Moon2.0',
+                  )
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 75),
+                color: Colors.pink[600],
+              )
+            ),
+
+            Text("you are currently learning " + types[learnerType],
+            style: TextStyle(
+              color: Colors.pink[900],
+              fontFamily: 'Moon2.0-Regular',
+              fontWeight: FontWeight.w400,
+              fontSize: 12,
+            )),
+            
+          ]
+        ),
+    );
+  }
+}
+
+// VISUAL EXERCISE -------------------------------------------------------
