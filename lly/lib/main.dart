0bc5d9996f977d7a3d0e3963db33e22518d4a2dc
Athena Siaotong <asiaotong@ucmerced.edu>
2020-02-16 02:19:59 -0800

    Added Moon 2.0 as the default font, finished splash screen

--- lly/lib/main.dart
+++ lly/lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Language Like You',
+      theme: ThemeData(fontFamily: 'Moon2.0'),
       home: Splash(),
     );
   }
@@ -19,22 +20,31 @@ class MyApp extends StatelessWidget {
 class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Column(
+    return Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 80, vertical: 250),
+        child: Column(
           children: <Widget>[
             // logo
             Image(
               image: AssetImage('assets/logo.png'),
             ),
-            FlatButton(
-              onPressed: () {
-                print("Pressed continue");
-              },
-              child: Text("Click me"),
-              color: Colors.pink[600],
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: FlatButton(
+                onPressed: () {
+                  print("Pressed continue");
+                },
+                child: Text(
+                  "continue",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Moon2.0',
+                  )
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 75),
+                color: Colors.pink[600],
+              ),
             ),
           ]
         ),
